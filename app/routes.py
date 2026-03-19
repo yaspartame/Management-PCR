4923150897b8e2cf8d64a765a577e86fb8128db2
Miyakazine <undyneleundying21@gmail.com>
2026-03-19 22:19:25 +0800

    Add bcrypt func

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for
 from app import app
 from app.models import get_db_connection
 
